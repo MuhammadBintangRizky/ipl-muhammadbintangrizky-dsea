59ca718a5c2a0683f8e5c3b8f06af550409aaecc
Muhammad Bintang Rizky <mbintang21@if.unjani.ac.id>
2023-10-18 16:49:50 +0700

    menambah code awal kasus liskov

---
+++ lsp/Smartphone.java
@@ -0,0 +1,14 @@
+package lsp;
+
+import java.sql.Date;
+
+public class Smartphone extends Product {
+    @Override
+    String getName(){
+        return "Samsung S10+ Limited Edition";
+    }
+    @Override
+    Date getExpiredDate(){
+        return new Date(0);
+    }
+}
