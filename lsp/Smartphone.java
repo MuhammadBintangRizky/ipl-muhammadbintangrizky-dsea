0ec4a53473fc693c40a85e0ec16288b73d3c17d0
Muhammad Bintang Rizky <mbintang21@if.unjani.ac.id>
2023-10-18 17:04:31 +0700

    perubahan sesuai prinsip liskov subs

--- lsp/Smartphone.java
+++ lsp/Smartphone.java
@@ -1,14 +1,9 @@
 package lsp;
 
-import java.sql.Date;
-
 public class Smartphone extends Product {
     @Override
     String getName(){
         return "Samsung S10+ Limited Edition";
     }
-    @Override
-    Date getExpiredDate(){
-        return new Date(0);
-    }
+   
 }
