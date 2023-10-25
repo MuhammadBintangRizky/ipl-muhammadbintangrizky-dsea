c33380f39b6e63e5c17b45ee65f0b672c50f9d40
Muhammad Bintang Rizky <mbintang21@if.unjani.ac.id>
2023-10-25 10:48:05 +0700

    create isp

---
+++ isp/Motorcyle.java
@@ -0,0 +1,22 @@
+package isp;
+
+public class Motorcyle implements VehicleInterface {
+    @Override
+    public void drive(){
+
+    }
+    @Override
+    public void stop(){
+
+    }
+    @Override
+    public void refuel(){
+
+    }
+
+    // this method can't implemented karena motor tidak punya pintu
+    @Override
+    public void openDoors(){
+
+    }
+}
