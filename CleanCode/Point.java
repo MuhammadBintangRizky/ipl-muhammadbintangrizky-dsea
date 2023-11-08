cfd3557e57f21bf922a4556e40bc926c65087545
Muhammad Bintang Rizky <mbintang21@if.unjani.ac.id>
2023-11-08 14:31:57 +0700

    perubahan point

--- CleanCode/Point.java
+++ CleanCode/Point.java
@@ -1,8 +1,10 @@
 package CleanCode;
 
-public class Point {
-    public class Point { 
-        public double x; 
-        public double y; 
-    } 
+public interface Point { 
+    double getX(); 
+    double getY(); 
+    void setCartesian(double x, double y); 
+    double getR(); 
+    double getTheta(); 
+    void setPolar(double r, double theta); 
 }
