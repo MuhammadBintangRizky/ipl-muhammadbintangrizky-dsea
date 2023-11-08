9d7a6a89c3c18ad869d4749ec3038c60346a7c6c
Muhammad Bintang Rizky <mbintang21@if.unjani.ac.id>
2023-11-08 11:00:01 +0700

    create customer

---
+++ CleanCode/Customer.java
@@ -0,0 +1,10 @@
+package CleanCode;
+
+public class Customer {
+    class DtaRcrd102 { 
+        private Date genymdhms; 
+        private Date modymdhms; 
+        private final String pszqint = "102"; 
+        /* ... */ 
+    } 
+}
