e031fc5b17f9db7c9c76308d0308a91052096eaf
Muhammad Bintang Rizky <mbintang21@if.unjani.ac.id>
2023-11-08 15:20:29 +0700

    perubahan exceptest

--- CleanCode/ExcepTest.java
+++ CleanCode/ExcepTest.java
@@ -4,11 +4,13 @@ import java.io.*;
 public class ExcepTest { 
     public static void main(String args[]) { 
       try { 
-        int a[] = new int[2]; 
-        System.out.println("Access element three :" + a[3]); 
-        } catch (ArrayIndexOutOfBoundsException e) { 
-           System.out.println("Exception thrown :" + e); 
-        } 
-        System.out.println("Out of the block"); 
-    } 
+         file = new FileInputStream(fileName); 
+         x = (byte) file.read(); 
+        } catch (IOException i) { 
+           i.printStackTrace(); 
+           return -1; 
+        } catch (FileNotFoundException f) // Not valid! { 
+           f.printStackTrace(); 
+           return -1; 
+    }         
 } 
