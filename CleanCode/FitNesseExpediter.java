8510daba5de7d46701701e83998b484edd91f7e0
Muhammad Bintang Rizky <mbintang21@if.unjani.ac.id>
2023-11-08 14:54:11 +0700

    perubahan formatting

--- CleanCode/FitNesseExpediter.java
+++ CleanCode/FitNesseExpediter.java
@@ -1,18 +1,14 @@
 package CleanCode;
 
-import java.io.InputStream;
-import java.net.Socket;
-
-public class FitNesseExpediter {
-    private   Socket            socket;
-    private   InputStream       input;
-    private   OutputStream      output;
-    private   Request           request;
-    private   Response          response;
-    private   FitNessContext    context;
-    protected long              requestParsingTimeLimit;
-    private   long              requestProgress;
-    private   long              requestParsingDeadline;
-    private   boolean           hassError;
-
+public class FitNesseExpediter implements ResponseSender { 
+    private Socket socket; 
+    private InputStream input; 
+    private OutputStream output; 
+    private Request request; 
+    private Response response; 
+    private FitNesseContext context; 
+    protected long requestParsingTimeLimit; 
+    private long requestProgress; 
+    private long requestParsingDeadline; 
+    private boolean hasError; 
 }
