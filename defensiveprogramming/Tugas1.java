64d850b8e4cf863983d075bbbba7c77360760789
Muhammad Bintang Rizky <mbintang21@if.unjani.ac.id>
2023-11-29 18:28:44 +0700

    create defensiveprogramming

---
+++ defensiveprogramming/Tugas1.java
@@ -0,0 +1,10 @@
+package defensiveprogramming;
+public class Tugas1 {
+    public static void main(String[] args) { 
+    try {
+          System.out.println(3/0);
+        } catch(Exception e) {
+    
+        }
+    }
+}
